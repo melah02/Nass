b2da05d81da8ea861444e087a4882f545f52d9cb
melah02 <melxymelah02@gmail.com>
2023-05-14 14:20:54 +0100

    upload button

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const first = document.getElementById('first');
 const second = document.querySelector('.second');
 const third = document.getElementById('third');
 const click = document.querySelectorAll('nav-link');
+const SubmitBtn = documnet.querySelector('submit');
+
+
+const fileUploaderDisplayButton = document.getElementById('upload');
+const fileContainer = document.getElementById('fileContainer');
 
 
 handBurgerMenu.addEventListener('click',function (){
@@ -13,6 +18,27 @@ handBurgerMenu.addEventListener('click',function (){
     third.classList.toggle('third1');
    
 });
+var uploadFileType  = (function() {
+    console.log('working')
+    var executed = false;
+    return function() {
+        if (!executed) {
+            executed = true;
+            const uploadInputType = document.createElement('input');
+            uploadInputType.type = 'file';
+            uploadInputType.classList = 'file';
+
+        fileContainer.appendChild(uploadInputType);
+        }
+    };
+})();
+
+Submit.addEventListener('click', function(e){
+    e.preventDefault();
+})
+
+fileUploaderDisplayButton.addEventListener('click', uploadFileType);
+
 
 
 
