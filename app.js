f75a05c1a315436db2fd793344f5c24a3f1e3b49
melah02 <melxymelah02@gmail.com>
2023-05-14 14:27:32 +0100

    submit bug

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const first = document.getElementById('first');
 const second = document.querySelector('.second');
 const third = document.getElementById('third');
 const click = document.querySelectorAll('nav-link');
-const SubmitBtn = documnet.querySelector('submit');
+const SubmitBtn = document.querySelector('submit');
 
 
 const fileUploaderDisplayButton = document.getElementById('upload');
@@ -19,7 +19,6 @@ handBurgerMenu.addEventListener('click',function (){
    
 });
 var uploadFileType  = (function() {
-    console.log('working')
     var executed = false;
     return function() {
         if (!executed) {
